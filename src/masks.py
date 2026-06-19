607fd35785f4aad8ba519c027b9864cbe9ab6474
Olesya <flomaster597@gmail.com>
2026-06-19 23:12:51 +0500

    commit

--- src/masks.py
+++ src/masks.py
@@ -2,13 +2,10 @@ def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер карты в формате XXXX XX** **** XXXX."""
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
-
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер счета в формате **XXXX."""
     return f"**{account_number[-4:]}"
 
 print(get_mask_card_number("1596837868705199"))
 print(get_mask_account("64686473678894779589"))
-
-
 
