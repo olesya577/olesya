48c0f81d3f7518ea771a6ce873072f3343dfb3b2
Olesya <flomaster597@gmail.com>
2026-06-30 02:21:30 +0500

    исправленный проект 2

--- src/masks.py
+++ src/masks.py
@@ -6,6 +6,3 @@ def get_mask_card_number(card_number: str) -> str:
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер счета в формате **XXXX."""
     return f"**{account_number[-4:]}"
-
-
-
--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,17 @@
-def filter_by_state(my_list_dir: list, state='EXECUTED') -> list:
+def filter_by_state(my_list_dir: list, state="EXECUTED") -> list:
     """Функция filter_by_state, которая принимает список словарей и опционально значение для ключа state (по умолчанию
     'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению."""
     filtered_list = []
     for dir in my_list_dir:
-        if dir['state'] == state:
+        if dir["state"] == state:
             filtered_list.append(dir)
     return filtered_list
+
 
 def sort_by_date(my_list_dir: list) -> list:
     """Функция sort_by_date, которая принимает список словарей и необязательный параметр, задающий
     порядок сортировки (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по
     дате (date)."""
-    sorted_list = sorted(my_list_dir, key=lambda employee: employee['date'], reverse=True)
+    sorted_list = sorted(my_list_dir, key=lambda employee: employee["date"], reverse=True)
     return sorted_list
